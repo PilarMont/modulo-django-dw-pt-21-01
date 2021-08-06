d31867597e0b215eb83300da2c9a1bb46e9b223d
Mauricio Chávez Olea <30444800+ultr4nerd@users.noreply.github.com>
2021-08-05 22:11:21 -0500

    Sesión 03

---
+++ sesion_03/music/models.py
@@ -0,0 +1,30 @@
+"""Music app models"""
+
+from django.db import models
+
+
+class Artist(models.Model):
+    """Artist model"""
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    """Album model"""
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Song(models.Model):
+    """Song model"""
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
